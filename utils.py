0594eb47825b073d14009b3003579967e5c5b61b
Carl <162527386+huangzexuan1022@users.noreply.github.com>
2025-07-30 15:52:28 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -17,8 +17,8 @@ def get_next_proxy(proxy_cycle):
     }
 
 
-# 选择不同的代理服务器去访问目标网站
-def fetch_with_proxy(url, params=None, retries=3, timeout=10, proxy_cycle=None):
+# 选择不同的代理服务器去访问目标网站，最多重试3次
+def fetch_with_proxy(url, params=None, retries=3, timeout=3, proxy_cycle=None):
 
     if proxy_cycle is None:
         raise ValueError("proxy_cycle 参数不能为空")
@@ -42,6 +42,22 @@ def fetch_with_proxy(url, params=None, retries=3, timeout=10, proxy_cycle=None):
     return None
 
 
+# 以Restful从币安获取K线数据，interval为K线周期
+def get_kline(symbol,interval,limit,proxy_cycle):
+    url = f"https://fapi.binance.com/fapi/v1/klines"
+    params = {"symbol": symbol, "interval": interval, "limit": limit}
+    return fetch_with_proxy(url, params, proxy_cycle=proxy_cycle)    
+
+
+# 计算数值数组最后count个值的平均值
+def calculate_recent_average(data: list[float], count: int) -> float:
+    if len(data) < count:
+        raise ValueError(f"数据长度不足 {count} 项")
+    recent_values = data[-count:]
+    return sum(recent_values) / count
+
+
+
 # 简单测试
 if __name__ == "__main__":
     symbols = ["BTCUSDT", "ETHUSDT", "SOLUSDT"]
@@ -53,3 +69,8 @@ if __name__ == "__main__":
         if price:
             print(f"{symbol} 合约最新价格：{price['price']}")
         time.sleep(1)
+
+    arr = [10, 20, 30, 40, 50]    
+    avg = calculate_recent_average(arr, 3)  # 计算最后3个值的平均值，即 (30+40+50)/3 = 40
+    print(avg)
+    
