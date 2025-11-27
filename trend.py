6f342d34739d42f470dff8af99896e7a2e4efacf
Bruce <149855130+zengzhuhong@users.noreply.github.com>
2025-11-27 20:26:09 +0800

    Update trend.py
    趋势判断由日K线改为4小时K线

--- trend.py
+++ trend.py
@@ -1,32 +1,32 @@
-import requests
 from utils import get_kline
 from itertools import cycle
 
-# 从日K线数据判断价格趋势，1表示上升趋势，-1表示下降趋势，0表示趋势未明
+# 从4小时K线数据判断价格趋势，1表示上升趋势，-1表示下降趋势，0表示趋势未明
 def trend(symbol, proxy_cycle)-> int:
-    daily = get_kline(symbol, interval="1d", limit=30, proxy_cycle=proxy_cycle)
-    if not daily or len(daily) < 30:
+
+    data = get_kline(symbol, interval="4h", limit=30, proxy_cycle=proxy_cycle)
+    if not data or len(data) < 30:
         print(f"❌ 无法获取 {symbol} 的K线数据")
         return 0
-    # 提取日K线数据中的收盘价
-    closes = [float(k[4]) for k in daily]
-    # 当天的价格MA14
+    # 提取K线数据中的收盘价
+    closes = [float(k[4]) for k in data]
+    # 当前时段的价格MA14
     window = closes[-14:]
     ma14 = sum(window) / 14
-    # print(f"今天的MA14: {ma14}")
-    # 前一天的价格MA14    
+    # print(f"最近一个时段的MA14: {ma14}")
+    # 前一个时段的价格MA14    
     window = closes[-15:-1]
     previous_ma14 = sum(window) / 14
-    # print(f"昨天的MA14: {previous_ma14}")
-    # 当天的价格MA7
+    # print(f"前一个时段的MA14: {previous_ma14}")
+    # 前一个时段的价格MA7
     window = closes[-7:]
     ma7 = sum(window) / 7
-    # print(f"今天的MA7: {ma7}")
-    # 前一天的价格MA7
+    # print(f"前一个时段的MA7: {ma7}")
+    # 前一个时段的价格MA7
     window = closes[-8:-1]
     previous_ma7 = sum(window) / 7
-    # print(f"昨天的MA7: {previous_ma7}")
-    # 当天的收盘价
+    # print(f"前一个时段的MA7: {previous_ma7}")
+    # 当前时段的收盘价
     current_close = closes[-1]
     # MA14和MA7同时上升，并且收盘价站在MA14之上
     if ma14 > previous_ma14 and ma7 > previous_ma7 and current_close > ma14:
